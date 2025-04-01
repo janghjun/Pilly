25b5b3ba73c1c1ff71a537c25fd5ab5fc7410ac7
janghjun <yyuyiy@naver.com>
2025-04-01 14:02:49 +0900

    Update FireBase

---
+++ example.tsx
@@ -0,0 +1,9 @@
+background-color: var(--color-background-default-default);
+  border-radius: var(--size-radius-200);
+  box-shadow: 0px 0px 5px 3px #aaaaaa80;
+  height: 281px;
+  left: 52px;
+  overflow: hidden;
+  position: absolute;
+  top: 317px;
+  width: 288px;
